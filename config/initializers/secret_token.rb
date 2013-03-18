d17424ec8c5e0897d9994b23875edc1bf1841e2d
www-data <www-data@ip-10-253-50-196.us-west-2.compute.internal>
2013-03-18 15:50:16 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '346cf2d7b2a08d5a4ca04e45764e883fdacb8071a8a20f8c667a40b177cf4338b1685f8d8cd8c3690027deae994875ac95646ba5d1ad83f0a32a51ec1b4d8009'
