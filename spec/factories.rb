9eea2494be169cc16a086c1e68ecaab8495cabdb
unicornzero <rylee.k@gmail.com>
2013-03-26 04:55:12 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "SomeotherMichael Hartl"
+    email    "someothermichael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+
