244215a7dd54b63594d2bccbd64e1b8eec37f1e7
unicornzero <rylee.k@gmail.com>
2013-03-28 02:53:37 +0000

    finish signup and test refactoring

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,16 @@
 include ApplicationHelper
 
 def valid_signin(user)
-  fill_in "Email", with: user.emai
+  fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+end
+
+def valid_signup(user)
+  fill_in "Name", with: user.name
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  fill_in "Confirmation", with: user.password_confirmation
 end
 
 RSpec::Matchers.define :have_error_message do |message|
@@ -13,12 +20,3 @@ RSpec::Matchers.define :have_error_message do |message|
 end
 
 
-## Deleted for Exercise 5.6
-#def full_title(page_title)
-#  base_title = "Ruby on Rails Tutorial Sample App"
-#  if page_title.empty?
-#    base_title
-#  else
-#    "#{base_title} | #{page_title}"
-#  end
-#end
